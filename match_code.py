b375a260a05d80c7506b5121123ce74164c9362c
DamsithKarunaratna <dammakaru@gmail.com>
2019-06-18 18:09:36 +0530

    Cleanup match_code.py

--- match_code.py
+++ match_code.py
@@ -1,8 +1,5 @@
-from PIL import Image # import the necessary packages
 from difflib import SequenceMatcher
-import pytesseract
 import argparse
-import cv2
 import os
 import time
 
@@ -18,59 +15,6 @@ def extract_timestamp(seconds):
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
     return h, m, s
-# ============================================================================================
-
-
-# def extract_text(mpath_to_File, mpreprocess):
-# 	# load the example image and convert it to grayscale
-# 	image = cv2.imread(mpath_to_File)
-# 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# 	# check to see if we should apply thresholding to preprocess the image
-# 	if mpreprocess == "thresh":
-# 		gray = cv2.threshold(gray, 0, 255,
-# 			cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-# 	# make a check to see if median blurring should be done to remove
-# 	# noise
-# 	elif mpreprocess == "blur":
-# 		gray = cv2.medianBlur(gray, 3)
-
-# 	# write the grayscale image to disk as a temporary file so we can
-# 	# apply OCR to it
-# 	filename = "{}.png".format(os.getpid())
-# 	cv2.imwrite(filename, gray)
-
-# 	# load the image as a PIL/Pillow image, apply OCR, and then delete
-# 	# the temporary file
-# 	text = pytesseract.image_to_string(Image.open(filename))
-# 	os.remove(filename)
-# 	return text
-
-# ============================================================================================
-
-# ============================================================================================
-
-# def write_to_disk(string, filename):
-# 	output_filename = filename.replace("jpg", "txt")
-# 	save_path = "./output"
-# 	if not (os.path.exists(save_path)):
-# 		try:  
-# 			os.mkdir(save_path)
-# 		except OSError:  
-# 			print ("Creation of the directory %s failed" % save_path)
-# 		else:  
-# 			print ("Successfully created the directory %s " % save_path)
-# 	with open(os.path.join(save_path, output_filename), "w") as ocr_file:
-# 		ocr_file.write(string)
-# 	return
-
-# ============================================================================================
-
-# ============================================================================================
-def process(line):
-    return
-
 # ============================================================================================
 
 # ============================================================================================
